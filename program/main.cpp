6cdbc70692450296f7104f3296df0976bb5704db
Mathias Tillman <master.homer@gmail.com>
2016-05-12 22:14:27 +0200

    Fix a potential VERY long wait.

--- program/main.cpp
+++ program/main.cpp
@@ -120,7 +120,7 @@ int main(int argc, char *argv[])
 
             if(retry) {
                 retries++;
-                QThread::sleep(1000);
+                QThread::sleep(5);
             }
         } while(retry);
 
