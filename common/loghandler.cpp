61cbe82c52a2f94e7ac1d3efaced995a073d5493
Mathias Tillman <master.homer@gmail.com>
2016-05-20 14:58:50 +0200

    Inlcude debug messages in the log sent to ultimobile in release mode.

--- common/loghandler.cpp
+++ common/loghandler.cpp
@@ -16,6 +16,13 @@ Log::LogHandlerWorker::~LogHandlerWorker() {
 }
 
 void Log::LogHandlerWorker::handleLog(const QString& line) {
+    // Skip debug messages when not in testing mode
+#ifndef TESTING_MODE
+    if(line.startsWith("[Debug]")) {
+        return;
+    }
+#endif
+
     QTextStream(stdout) << line;
     if(mFile->isOpen()) {
         QTextStream stream(mFile);
@@ -48,12 +55,11 @@ Log::LogHandler::~LogHandler() {
 
 void Log::LogHandler::addLog(QtMsgType type, const QMessageLogContext &context, QString msg) {
     QString line = formatLog(type, context, msg);
-#ifdef QT_DEBUG
+    if(line.length() == 0) {
+        return;
+    }
+
     emit log(line);
-#else
-    Q_UNUSED(context);
-    emit log(line);
-#endif
 }
 
 void Log::LogHandler::handleLog(const QString& data) {
@@ -68,13 +74,6 @@ void Log::LogHandler::handleLog(const QString& data) {
 QString Log::LogHandler::formatLog(QtMsgType type, const QMessageLogContext& context, QString msg) {
 #ifndef QT_DEBUG
     Q_UNUSED(context);
-#endif
-
-    // Skip debug messages when not in testing mode
-#ifndef TESTING_MODE
-    if(type == QtDebugMsg) {
-        return "";
-    }
 #endif
     QString data = "";
     switch(type) {
